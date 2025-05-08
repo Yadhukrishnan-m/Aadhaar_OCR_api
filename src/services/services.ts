f285ae31279ae6c3aa705ee3ecddf6c0e028063d
Yadhukrishnan-m <yadhumon2003@gmail.com>
2025-05-09 01:18:09 +0530

    credential changed to env

--- src/services/services.ts
+++ src/services/services.ts
@@ -2,15 +2,34 @@ import { ImageAnnotatorClient } from "@google-cloud/vision";
 import path from "path";
 import { Buffer } from "buffer";
 import dotenv from "dotenv";
+import { CustomError } from "../middleware/errorHandler";
 
 dotenv.config();
 // Create the Vision client with your credentials file
+
+// if (!process.env.GOOGLE_PRIVATE_KEY) {
+//     throw new CustomError("ENV not loaded",500)
+// }   
+
+console.log();
+
+const credentials = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY,
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+  universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+};
+// Create the Vision client with the embedded credentials
 const client = new ImageAnnotatorClient({
-  keyFilename: path.join(__dirname, "vision-key.json"),
+  credentials: credentials, // Use the embedded credentials
 });
-// Log the environment variables (but not sensitive ones like private_key)
-
-
 // Function to perform OCR on an image buffer
 export async function google_Vision_OCR(imageBuffer: Buffer): Promise<string | undefined | null> {
   try {
